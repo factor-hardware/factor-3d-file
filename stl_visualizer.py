797c87bc092064a095dae5c16c2267051b841e5a
ChronoAndross <Andross928@gmail.com>
2022-08-31 12:28:15 -0400

    create a stl visualizer, will come in handy for FEA analysis

---
+++ stl_visualizer.py
@@ -0,0 +1,49 @@
+
+import sys
+import vtk
+import os
+import platform
+import pyvista as pv
+from pyvirtualdisplay import Display
+
+#def bubble_wrap_calc(bubble_wrap_size, mesh, iter_num):
+#    is_equal = False
+#    for i in range(1, iter_num):
+#        # perform calculations with bubble_wrap and mesh
+#        # grab results from FEA analsys
+#        equal_forces = result.fea_analysis_force
+#        if abs(equal_forces) < 1:
+#            is_equal = True
+#            break
+#        
+#    return is_equal
+def visualize_vtk(vtk_mesh):
+    display = Display(visible=0, size=(1280, 1024))
+    display.start()
+    plotter = pv.Plotter()
+    plotter.add_mesh(vtk_mesh, show_edges=True)
+    plotter.show(window_size=[512, 384], cpos="iso")
+    display.stop()
+
+def create_vtk(stl_mesh_path : str):
+    out_vtk = None
+    if '.stl' in stl_mesh_path:
+        render_vtk = vtk.vtkSTLReader()
+        render_vtk.SetFileName(stl_mesh_path)
+        render_vtk.Update()
+        out_vtk = render_vtk.GetOutput()
+    return out_vtk
+
+def setup_for_platform():
+    if platform.system() == 'Darwin':
+        os.environ["PATH"] += os.pathsep + '/opt/X11/bin'
+
+if __name__ == "__main__":
+    stl_mesh_path = str(sys.argv[1])
+    setup_for_platform()
+    vtk_mesh = create_vtk(stl_mesh_path)
+    if vtk_mesh:
+        print('visualizing')
+        visualize_vtk(vtk_mesh)
+    else:
+        print('file invalid, visualization failed')
